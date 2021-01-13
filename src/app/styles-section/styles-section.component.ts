bcfaefa48bee3c3a6baf7b3bafe411c8e70c2937
Nikita Chernega <nikita.chernega1@gmail.com>
2021-01-13 18:19:33 +0200

    feat(app.module) base setup

---
+++ src/app/styles-section/styles-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-styles-section',
+  templateUrl: './styles-section.component.html',
+  styleUrls: ['./styles-section.component.scss']
+})
+export class StylesSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/templates-section/templates-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-templates-section',
+  templateUrl: './templates-section.component.html',
+  styleUrls: ['./templates-section.component.scss']
+})
+export class TemplatesSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
